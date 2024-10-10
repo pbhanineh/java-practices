197870104b2350be914e3b0deecef2f0cdad3fd8
pbhanineh <bhaninehpaul12@gmail.com>
2024-10-10 22:06:56 +0200

    commit

--- longest-distinct-substring/src/Main.java
+++ longest-distinct-substring/src/Main.java
@@ -3,28 +3,33 @@ import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
-        String result = findLongestSubstring("abcdemorrap");
+        String result = longestDistinctSubstring("abcdemorrspkt");
         System.out.println(result);
     }
-        private static String findLongestSubstring(String s) {
-                Set<Character> charsInWindows = new HashSet<>();
-                int start = 0;
-                int maxLength = 0;
-                int maxStart = 0;
 
-for (int end = 0; end < s.length(); end++) {
-    char currentChar = s.charAt(end);
-    while (charsInWindows.contains(currentChar)) {
-        charsInWindows.remove(s.charAt(start));
-        start ++;
-    }
-    charsInWindows.add(currentChar);
-    if(end - start > maxLength) {
-        maxLength = end - start + 1;
-        maxStart = start;
-    }
-}
-return s.substring(maxStart, maxStart + maxLength);
 
+    private static String longestDistinctSubstring(String s) {
+
+        int end = 0, start = 0;
+        int maxLength = 0;
+        int maxStart = 0;
+        Set<Character> charInSet = new HashSet<>();
+
+        for (end = 0; end < s.length(); end++) {
+            char currentChar = s.charAt(end);
+
+            // Si le caractère actuel est déjà dans le Set, on réduit la fenêtre
+            while (charInSet.contains(currentChar)) {
+                charInSet.remove(s.charAt(start));  // Supprimer le caractère au début de la fenêtre
+                start++;  // Avancer le pointeur "start"
+            }
+
+            charInSet.add(currentChar);
+            if (end - start + 1 > maxLength) {
+                maxLength = end - start + 1;
+                maxStart = start;
+            }
         }
+        return s.substring(maxStart, maxStart + maxLength);
+    }
 }
